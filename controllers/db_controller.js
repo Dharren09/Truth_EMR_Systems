d7004533608f0096adb1358d77cda925955dccf2
Dharren09 <dharrydharrenzerah@gmail.com>
2023-06-20 13:55:21 +0300

    connecting TruthMD to sql

---
+++ controllers/db_controller.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+var con = mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password: 'Dharrenz1996',
+    database: 'truthmd'
+});
+
+con.connect(function(err){
+    if(err){
+	throw err;
+    }else{
+	console.log('Successfully connected to TruthMD Database')
+    }
+});
